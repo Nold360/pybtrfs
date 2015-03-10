28cced50adfb32443e61e61325b938e781a6ab8a
Matt Grant <matt.grant@foodstuffs-si.co.nz>
2015-03-11 09:58:19 +1300

    Evaluate BLOCK before doing btrfs metadata checks
    - Command line help and debug switches
    - Put code in to do conifguration file
    - Check that BLOCK used on a btrfs is greater than 90% before
      checking METADATA and SYSTEM percentages

--- linux_fs_check/linux_fs_check_new.sh
+++ linux_fs_check/linux_fs_check_new.sh
@@ -16,16 +16,16 @@
 #MAILTO="matt.grant@foodstuffs-si.co.nz"
 MAILTO="itbasissupport@foodstuffs-si.co.nz"
 MAILTO_DEBUG="matt.grant@foodstuffs-si.co.nz"
-DEBUG=""       # set to any value to turn debug on
+DEBUG=""      # set to any value to turn debug on
 IGNORE_BLOCK="" # set if you want to ignore /BLOCK output
 TIME=`date`
 HOSTNAME=`hostname`
 
-
+#==============Config file here================
 # skip checking any filesystems in this list
 SKIP_LIST="/proc"
-
-DF_MAX_PERCENT=90  # default warning value
+# Default warning value
+DF_MAX_PERCENT=90
 # specially check any filesystems in this list against given value
 # format is "/filesystem,80" additional entries should be on a new line
 DF_EXCEPTION_LIST=""
@@ -33,19 +33,64 @@ DF_EXCEPTION_LIST=""
 DF_BTRFS_CHECK_ALL="--btrfs-all"
 #DF_BTRFS_CHECK_ALL=""
 # BTRFS max metadata %
-BTRFS_MAX_METADATA_PERCENT=77
+BTRFS_MAX_METADATA_PERCENT=75
 # BTRFS max system %
-BTRFS_MAX_SYSTEM_PERCENT=77
+BTRFS_MAX_SYSTEM_PERCENT=75
+# BTRFS BLOCK check waterline
+BTRFS_BLOCK_CHECK_THRESHOLD=90
+#==============Config file here================
 
-[ -n "$DEBUG" ] && DF_MAX_PERCENT=5
-[ -n "$DEBUG" ] && BTRFS_MAX_METADATA_PERCENT=5
-[ -n "$DEBUG" ] && BTRFS_MAX_SYSTEM_PERCENT=5
-[ -n "$DEBUG" ] && MAILTO="$MAILTO_DEBUG"
+CFG_FILE="/etc/default/linux_fs_check_new"
+[ -f "$CFG_FILE" ] && . "$CFG_FILE" || true
 
 PATH="$PATH:/sbin:/usr/sbin"
 DF="/usr/local/bin/pybtrfs"
 DF_SUBARG="df"
 PROGNAME=`basename $0`
+
+usage () {
+	echo 1>&2
+	echo "Usage: $PROGNAME [-dh]" 1>&2
+	echo 1>&2
+	echo "                  -d     turn on debug" 1>&2
+	echo "                  -h     display this help" 1>&2
+	echo 1>&2
+	echo "                  Config file is '${CFG_FILE}'" 1>&2
+	echo 1>&2
+	exit 1
+}
+
+OPTIND=1
+while getopts dh F; do
+	case $F in
+	d)	DEBUG="1"
+		;;
+	h)	usage
+		exit 1
+		;;
+	\?)
+		usage
+		exit 1
+		;;
+	*)
+		usage
+		exit 1
+		;;
+esac
+done
+shift $(( $OPTIND - 1 ))
+
+if [ $# -ge 1 ]; then
+	# Deal with any arguments left that are not switches
+	usage
+	exit 1
+fi
+
+[ -n "$DEBUG" ] && DF_MAX_PERCENT=5
+[ -n "$DEBUG" ] && BTRFS_MAX_METADATA_PERCENT=5
+[ -n "$DEBUG" ] && BTRFS_MAX_SYSTEM_PERCENT=5
+[ -n "$DEBUG" ] && BTRFS_BLOCK_CHECK_THRESHOLD=90
+[ -n "$DEBUG" ] && MAILTO="$MAILTO_DEBUG"
 
 if ! [ -x "${DF}" ]; then
 	echo "${PROGNAME}: Can't find '${DF}' or it is not executable." 1>&2
@@ -73,7 +118,8 @@ THRESHOLD:\tTrigger Point is ${THRESHOLD}%"
 check_df ()
 # look for match with EXCEPTION_LIST
 {
-	local DF_PERCENT MAX FS SIZE AVAILABLE USE_PERC MOUNT_PT
+	local DF_PERCENT MAX FS SIZE AVAILABLE USE_PERC MOUNT_PT BLOCK_USED_PERCENT
+	local BLK_PERC BLK_CHECK_PATH
 
 	while read FS SIZE USED AVAILABLE USE_PERC MOUNT_PT; do  
 
@@ -81,18 +127,36 @@ check_df ()
 			continue
 		fi
 
+		# Put BLOCK percent used into an array
+		if echo "$MOUNT_PT" | grep -q "\/BLOCK$"; then
+			BLOCK_USED_PERCENT="${BLOCK_USED_PERCENT}${MOUNT_PT} ${USE_PERC}
+"
+		fi
+
 		# skip BTRFS DATA - checked under df
 		if echo "$MOUNT_PT" | grep -q "\/DATA$"; then
 			continue
 		fi
-		# skip BTRFS DATA - checked under df
+		# skip BTRFS BLOCK - checked under df
 		if [ -n "$IGNORE_BLOCK" ] && echo "$MOUNT_PT" | grep -q "\/BLOCK$"; then
 			continue
 		fi
 		# skip if fs on exclusion list
-		if echo "$MOUNT_PT" | grep -q "$SKIP_LIST"; then
+		if echo "$SKIP_LIST" | grep -q "$MOUNT_PT"; then
 			if [ $DEBUG ]; then echo "$1 on Skip list -ignoring"; fi
 			continue
+		fi
+		# Skip BTRFS checks if there is unallocated BLOCK available
+		if echo "$MOUNT_PT" | grep -q 'SYSTEM$\|METADATA$'; then
+			BLK_CHECK_PATH=`echo "$MOUNT_PT" | perl -pe 's/^(\S*\/)(SYSTEM$|METADATA$)/\1/'`
+			[ "$BLK_CHK_PATH" != '/' ] && BLK_CHECK_PATH=`echo "$BLK_CHECK_PATH" | perl -pe 's/^(\S+)\//\1/'`
+			BLK_PERC=`echo "$BLOCK_USED_PERCENT" | grep "$BLK_CHECK_PATH" | perl -pe 's/^\S+\s+(\S+)$/\1/' | cut -d "%" -f 1`
+			[ $DEBUG ] && echo "BLK_CHECK_PATH: $BLK_CHECK_PATH"
+			[ $DEBUG ] && echo "BLK_PERC: $BLK_PERC"
+			if [ $BLK_PERC -le $BTRFS_BLOCK_CHECK_THRESHOLD ]; then
+				[ $DEBUG ] && echo "Skipping btrfs $MOUNT_PT as there is unused BLOCK available - $BLK_PERC <= $BTRFS_BLOCK_CHECK_THRESHOLD"
+				continue
+			fi
 		fi
 
 		DF_PERCENT=`echo ${USE_PERC} | cut -d "%" -f1`
